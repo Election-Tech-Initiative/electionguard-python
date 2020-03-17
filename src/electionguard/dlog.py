67ea1da7cfec8ef3191dd57b2df126bfb23f9566
Dan Wallach <dwallach@rice.edu>
2020-03-17 11:20:00 -0500

    dlog: now thread-safe

--- src/electionguard/dlog.py
+++ src/electionguard/dlog.py
@@ -1,5 +1,6 @@
 # support for computing discrete logs, with a cache so they're never recomputed
 
+import asyncio
 from typing import Dict
 
 from .group import G, ElementModP, ONE_MOD_P, mult_mod_p
@@ -7,27 +8,42 @@ from .group import G, ElementModP, ONE_MOD_P, mult_mod_p
 __dlog_cache: Dict[ElementModP, int] = {ONE_MOD_P: 0}
 __dlog_max_elem = ONE_MOD_P
 __dlog_max_exp = 0
+__dlog_lock = asyncio.Lock()
 
 
 def discrete_log(e: ElementModP) -> int:
     """
     Computes the discrete log (base g, mod p) of the given element,
-    with internal caching of results. The exponent is expected
-    to fit within [0, Q).
-    """
+    with internal caching of results. Should run efficiently when called
+    multiple times when the exponent is at most in the single-digit millions.
+    Performance will degrade if it's much larger.
 
+    Note: *this function is thread-safe*. For the best possible performance,
+    pre-compute the discrete log of a number you expect to have the biggest
+    exponent you'll ever see. After that, the cache will be fully loaded,
+    and every call will be nothing more than a dictionary lookup.
+    """
+    global __dlog_cache
+
+    # no need for mutually exclusive access when reading from the cache
+    if e in __dlog_cache:
+        return __dlog_cache[e]
+    else:
+        return asyncio.run(__discrete_log_internal(e))
+
+
+async def __discrete_log_internal(e: ElementModP) -> int:
     global __dlog_cache
     global __dlog_max_elem
     global __dlog_max_exp
+    global __dlog_lock
 
-    g = ElementModP(G)
+    async with __dlog_lock:
+        g = ElementModP(G)
 
-    if e in __dlog_cache:
-        return __dlog_cache[e]
+        while e != __dlog_max_elem:
+            __dlog_max_exp = __dlog_max_exp + 1
+            __dlog_max_elem = mult_mod_p(g, __dlog_max_elem)
+            __dlog_cache[__dlog_max_elem] = __dlog_max_exp
 
-    while e != __dlog_max_elem:
-        __dlog_max_exp = __dlog_max_exp + 1
-        __dlog_max_elem = mult_mod_p(g, __dlog_max_elem)
-        __dlog_cache[__dlog_max_elem] = __dlog_max_exp
-
-    return __dlog_cache[__dlog_max_elem]
+        return __dlog_cache[__dlog_max_elem]
