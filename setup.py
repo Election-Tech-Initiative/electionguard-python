8bb9267a517c19e160ae1f6a3014ee673ae9218a
Keith Fung <keith.fung@infernored.com>
2020-04-24 11:10:00 -0400

    ✅ Configure Pytest for VS Code
    __init__ is necessary to ensure tests are run properly.

--- setup.py
+++ setup.py
@@ -3,93 +3,58 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-import io
 from glob import glob
 from os.path import basename
-from os.path import dirname
-from os.path import join
 from os.path import splitext
 
 from setuptools import find_packages
 from setuptools import setup
 
+NAME = "electionguard"
+VERSION = "0.1.0"
+LICENSE = "MIT"
+DESCRIPTION = "ElectionGuard: Support for e2e verified elections."
+AUTHOR = "Dan S. Wallach"
+AUTHOR_EMAIL = "dwallach@gmail.com"
+URL = "https://github.com/microsoft/electionguard-python"
+PROJECT_URLS = {
+    "Changelog": "https://github.com/microsoft/electionguard-python/blob/master/CHANGELOG.rst",
+    "Issue Tracker": "https://github.com/microsoft/electionguard-python/issues",
+}
+CLASSIFIERS = [
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: Unix",
+    "Operating System :: POSIX",
+    "Operating System :: Microsoft :: Windows",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Utilities",
+]
 
-# Borrowed from here: https://blog.ionelmc.ro/2014/05/25/python-packaging/
-
-
-def read(*names, **kwargs):
-    with io.open(
-        join(dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")
-    ) as fh:
-        return fh.read()
-
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
 
 setup(
-    name="electionguard",
-    version="0.1.0",
-    license="MIT",
-    description="ElectionGuard: support for e2e verified elections.",
-    # long_description='%s\n%s' % (
-    #     re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
-    #     re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
-    # ),
-    author="Dan S. Wallach",
-    author_email="dwallach@gmail.com",
-    url="https://github.com/microsoft/electionguard-python",
+    name=NAME,
+    version=VERSION,
+    license=LICENSE,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
     packages=find_packages("src"),
     package_dir={"": "src"},
     py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
-    classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: Unix",
-        "Operating System :: POSIX",
-        "Operating System :: Microsoft :: Windows",
-        "Programming Language :: Python",
-        # 'Programming Language :: Python :: 2.7',
-        # 'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3.5',
-        # 'Programming Language :: Python :: 3.6',
-        # 'Programming Language :: Python :: 3.7',
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: Implementation :: CPython",
-        # "Programming Language :: Python :: Implementation :: PyPy",
-        # uncomment if you test on these interpreters:
-        # 'Programming Language :: Python :: Implementation :: IronPython',
-        # 'Programming Language :: Python :: Implementation :: Jython',
-        # 'Programming Language :: Python :: Implementation :: Stackless',
-        "Topic :: Utilities",
-    ],
-    project_urls={
-        "Changelog": "https://github.com/microsoft/electionguard-python/blob/master/CHANGELOG.rst",
-        "Issue Tracker": "https://github.com/microsoft/electionguard-python/issues",
-    },
-    keywords=[
-        # eg: 'keyword1', 'keyword2', 'keyword3',
-    ],
+    classifiers=CLASSIFIERS,
+    project_urls=PROJECT_URLS,
     python_requires=">=3.8",
-    test_suite='tests',
-    tests_require=["hypothesis==5.9.0"],
-    install_requires=[
-        'gmpy2==2.1.0b4',
-        "numpy==1.18.2",  # only used for the benchmarks, so maybe doesn't need to be here?
-        'typish>=1.3.1',
-        'jsons==1.1.2'
-        # eg: 'aspectlib==1.1.1', 'six>=1.7',
-    ],
-    extras_require={
-        # eg:
-        #   'rst': ['docutils>=0.11'],
-        #   ':python_version=="2.6"': ['argparse'],
-    },
-    setup_requires=["pytest-runner",],
-    entry_points={
-        #         'console_scripts': [
-        #             'nameless = nameless.cli:main',
-        #        ]
-    },
+    install_requires=["gmpy2==2.1.0b4"],
 )
