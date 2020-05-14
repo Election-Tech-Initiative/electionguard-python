c41e646a363d791312edf94b5a4a1ed57b0e7954
Dan Wallach <dwallach@rice.edu>
2020-05-14 17:46:21 -0500

    fixed type error in test_decrypt, but the bug is still there

--- tests/test_decrypt.py
+++ tests/test_decrypt.py
@@ -21,7 +21,8 @@ from electionguard.decrypt import (
     decrypt_ballot_with_secret,
 )
 
-from electionguard.election import ContestDescription, SelectionDescription
+from electionguard.election import ContestDescription, SelectionDescription, generate_placeholder_selections_from, \
+    contest_description_with_placeholders_from
 
 from electionguard.elgamal import ElGamalKeyPair
 
@@ -163,16 +164,19 @@ class TestDecrypt(unittest.TestCase):
         _, description = contest_description
         data = ballot_factory.get_random_contest_from(description)
 
+        placeholders = generate_placeholder_selections_from(description)
+        description_with_placeholders = contest_description_with_placeholders_from(description, placeholders)
+
         # Act
-        subject = encrypt_contest(data, description, keypair.public_key, seed)
+        subject = encrypt_contest(data, description_with_placeholders, keypair.public_key, seed)
         result_from_key = decrypt_contest_with_secret(
-            subject, description, keypair.public_key, keypair.secret_key
+            subject, description_with_placeholders, keypair.public_key, keypair.secret_key
         )
         result_from_nonce = decrypt_contest_with_nonce(
-            subject, description, keypair.public_key
+            subject, description_with_placeholders, keypair.public_key
         )
         result_from_nonce_seed = decrypt_contest_with_nonce(
-            subject, description, keypair.public_key, seed
+            subject, description_with_placeholders, keypair.public_key, seed
         )
 
         # Assert
