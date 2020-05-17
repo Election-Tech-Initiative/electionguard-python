a8e86117bf16f4ee20cf99c2d4e761e123eaf915
Dan Wallach <dwallach@rice.edu>
2020-05-16 20:47:24 -0500

    sometimes we hit the time limit

--- tests/test_encrypt_hypotheses.py
+++ tests/test_encrypt_hypotheses.py
@@ -1,6 +1,7 @@
 import unittest
+from datetime import timedelta
 
-from hypothesis import given
+from hypothesis import given, HealthCheck, settings
 
 from electionguard.election import ElectionDescription
 from electionguardtest.election import arb_election_description
@@ -12,6 +13,11 @@ class TestContests(unittest.TestCase):
 
 
 class TestElections(unittest.TestCase):
+    @settings(
+        deadline=timedelta(milliseconds=2000),
+        suppress_health_check=[HealthCheck.too_slow],
+        max_examples=10,
+    )
     @given(arb_election_description())
     def test_generators_yield_valid_output(self, ed: ElectionDescription):
         self.assertTrue(ed.is_valid())
