5aef4e197ae2b9a6e085b3d481e31c20df9f0f45
Keith Fung <keith.fung@infernored.com>
2020-06-04 16:46:43 -0400

    ✨Add Mediator and Test File

---
+++ tests/test_mediator.py
@@ -0,0 +1,73 @@
+from unittest import TestCase
+
+
+class TestMediator(TestCase):
+    def test_reset(self):
+        pass
+
+    # Attendance
+    def test_confirm_presence_of_guardian(self):
+        pass
+
+    def test_all_guardians_in_attendance(self):
+        pass
+
+    def test_share_guardians_in_attendance(self):
+        pass
+
+    # Auxiliary Public Keys
+    def test_receive_auxiliary_public_key(self):
+        pass
+
+    def test_all_auxiliary_public_keys_available(self):
+        pass
+
+    def test_share_auxiliary_public_keys(self):
+        pass
+
+    # Election Public Keys
+    def test_receive_election_public_key(self):
+        pass
+
+    def test_all_election_public_keys_available(self):
+        pass
+
+    def test_share_election_public_keys(self):
+        pass
+
+    # Election Partial Key Backups
+    def test_receive_election_partial_key_backup(self):
+        pass
+
+    def test_all_election_partial_key_backups_received(self):
+        pass
+
+    def test_share_election_partial_key_backups_to_guardian(self):
+        pass
+
+    # Partial Key Verifications
+    def test_receive_election_partial_key_verification(self):
+        pass
+
+    def test_all_election_partial_key_verifications_received(self):
+        pass
+
+    def test_all_election_partial_key_verified(self):
+        pass
+
+    # Partial Key Challenges
+    def test_share_failed_partial_key_verifications(self):
+        pass
+
+    def test_share_missing_election_partial_key_challenges(self):
+        pass
+
+    def test_receive_election_partial_key_challenge(self):
+        pass
+
+    def test_share_open_election_partial_key_challenges(self):
+        pass
+
+    # Publish Joint Key
+    def test_publish_joint_key(self):
+        pass
