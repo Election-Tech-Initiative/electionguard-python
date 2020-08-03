a565e1e21ee58f41c46e3845e4415a9d38682574
Dan Wallach <dwallach@rice.edu>
2020-08-03 10:40:07 -0500

    fixed async issue when no event loop

--- src/electionguard/dlog.py
+++ src/electionguard/dlog.py
@@ -1,14 +1,16 @@
 # support for computing discrete logs, with a cache so they're never recomputed
 
 import asyncio
-from typing import Dict
+from typing import Dict, Optional
 
 from .group import G, ElementModP, ONE_MOD_P, mult_p, int_to_p_unchecked
 
 __dlog_cache: Dict[ElementModP, int] = {ONE_MOD_P: 0}
 __dlog_max_elem = ONE_MOD_P
 __dlog_max_exp = 0
-__dlog_lock = asyncio.Lock()
+
+# initialized inside __discrete_log_internal
+__dlog_lock: Optional[asyncio.Lock] = None
 
 
 def discrete_log(e: ElementModP) -> int:
@@ -37,6 +39,18 @@ async def __discrete_log_internal(e: ElementModP) -> int:
     global __dlog_max_elem
     global __dlog_max_exp
     global __dlog_lock
+
+    if __dlog_lock is None:
+        # We cannot run asyncio.Lock() if we don't have an "event loop", which
+        # can happen if we're running in certain environments. The solution
+        # is to put this bit of initialization into an async function, which
+        # is where we happen to be right now. With Python's relatively simple
+        # concurrency model, we don't have to worry about multiple threads
+        # arriving here simultaneously. This code will run exactly once. If
+        # you're using one of the many multiprocessing libraries, this will
+        # run exactly once per process.
+
+        __dlog_lock = asyncio.Lock()
 
     async with __dlog_lock:
         g = int_to_p_unchecked(G)
