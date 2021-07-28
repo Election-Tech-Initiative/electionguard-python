e0cd9c72f38a08d278c12693008dba5024134106
Keith Fung <keith.robert.fung@gmail.com>
2021-07-28 18:19:58 -0400

    ♻️ Convert EncryptionDevice to dataclass

--- src/electionguard/encrypt.py
+++ src/electionguard/encrypt.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from dataclasses import dataclass
 from typing import List, Optional
 from uuid import getnode
 
@@ -29,6 +30,7 @@ from .nonces import Nonces
 from .utils import get_optional, get_or_else_optional_func
 
 
+@dataclass
 class EncryptionDevice:
     """
     Metadata for encryption device
@@ -45,20 +47,6 @@ class EncryptionDevice:
 
     location: str
     """Arbitary string to designate the location of device"""
-
-    def __init__(
-        self,
-        device_id: int,
-        session_id: int,
-        launch_code: int,
-        location: str,
-    ) -> None:
-        self.device_id = device_id
-        self.session_id = session_id
-        self.launch_code = launch_code
-        self.location = location
-
-        log_info(f": EncryptionDevice: Created: UUID: {device_id} at: {location}")
 
     def get_hash(self) -> ElementModQ:
         """
