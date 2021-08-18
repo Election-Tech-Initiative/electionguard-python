9cc7255e99ee43e851e75620f7ba5e02365fe949
Keith Fung <keith.robert.fung@gmail.com>
2021-08-18 13:19:35 -0400

    ♻️  Sequence Order Base object and sorting method
    - Sequence Order sorting method
    - Base Object to identify objects that contain sequence order

--- src/electionguard/election_object_base.py
+++ src/electionguard/election_object_base.py
@@ -1,4 +1,7 @@
+"""Base objects to derive other election objects."""
+
 from dataclasses import dataclass
+from typing import List, TypeVar
 
 
 @dataclass
@@ -6,3 +9,24 @@ class ElectionObjectBase:
     """A base object to derive other election objects identifiable by object_id."""
 
     object_id: str
+
+
+@dataclass
+class OrderedObjectBase(ElectionObjectBase):
+    """A ordered base object to derive other election objects."""
+
+    sequence_order: int
+    """
+    Used for ordering in a ballot to ensure various encryption primitives are deterministic.
+    The sequence order must be unique and should be representative of how the items are represented
+    on a template ballot in an external system.  The sequence order is not required to be in the order
+    in which they are displayed to a voter.  Any acceptable range of integer values may be provided.
+    """
+
+
+_Orderable_T = TypeVar("_Orderable_T", bound="OrderedObjectBase")
+
+
+def sequence_order_sort(unsorted: List[_Orderable_T]) -> List[_Orderable_T]:
+    """Sort by sequence order."""
+    return sorted(unsorted, key=lambda item: item.sequence_order)
