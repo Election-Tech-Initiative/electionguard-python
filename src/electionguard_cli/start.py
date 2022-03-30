a63492711340206763b8420f9f0843297fcf4494
Lee Richardson <github@leerichardson.com>
2022-03-30 16:30:08 -0400

    Pull manifest, display its data

--- src/electionguard_cli/start.py
+++ src/electionguard_cli/start.py
@@ -1,13 +1,33 @@
+from email.policy import default
 import click
+from electionguard import guardian
+from electionguard.manifest import Manifest
+from electionguard_tools.factories.election_factory import (
+    ElectionFactory,
+    NUMBER_OF_GUARDIANS,
+)
 
 @click.command()
-@click.option('--count', help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo(f"Hello {name}!")
+@click.option('--guardian-count', prompt='Number of guardians', help='The number of guardians that will participate in the key ceremony and tally.')
+@click.option('--quorum', prompt='Quorum', help='The minimum number of guardians required to show up to the tally.')
+def start(guardian_count, quorum):
+    """Runs through an end-to-end election."""
+
+    manifest: Manifest = ElectionFactory().get_simple_manifest_from_file()
+    click.echo(f"{'-'*40}")
+    click.echo("Election Summary:")
+    manifest_name = manifest.name.text[0].value
+    click.echo(f"Name: {manifest_name}")
+    click.echo(f"Scope: {manifest.election_scope_id}")
+    click.echo(f"Geopolitical Units: {len(manifest.geopolitical_units)}")
+    click.echo(f"Parties: {len(manifest.parties)}")
+    click.echo(f"Candidates: {len(manifest.candidates)}")
+    click.echo(f"Contests: {len(manifest.contests)}")
+    click.echo(f"Ballot Styles: {len(manifest.ballot_styles)}")
+    click.echo(f"Guardians: {guardian_count}")
+    click.echo(f"Quorum: {quorum}")
+    click.echo(f"{'-'*40}\n")
+
 
 if __name__ == '__main__':
-    hello()
+    start()
