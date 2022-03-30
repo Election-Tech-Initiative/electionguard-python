10538b72d65e743e55f7a49053de2f4082301746
Lee Richardson <github@leerichardson.com>
2022-03-30 10:32:46 -0400

    poetry add click, to help with CLI work

--- src/electionguard_cli/start.py
+++ src/electionguard_cli/start.py
@@ -1,2 +1,13 @@
-val = input("How many guardians? ")
-print(f"Ok, adding {val} guardians")
+import click
+
+@click.command()
+@click.option('--count', help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+def hello(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo(f"Hello {name}!")
+
+if __name__ == '__main__':
+    hello()
