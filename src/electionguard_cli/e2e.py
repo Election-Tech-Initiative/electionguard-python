c2f3e1eaa8fbefe40ae343ba4d575120b0cfe0a6
Lee Richardson <github@leerichardson.com>
2022-03-30 17:18:21 -0400

    Add output formatting and colors

--- src/electionguard_cli/e2e.py
+++ src/electionguard_cli/e2e.py
@@ -6,25 +6,33 @@ from electionguard_tools.factories.election_factory import (
 )
 
 @click.command()
-@click.option('--guardian-count', prompt='Number of guardians', help='The number of guardians that will participate in the key ceremony and tally.')
-@click.option('--quorum', prompt='Quorum', help='The minimum number of guardians required to show up to the tally.')
+@click.option('--guardian-count', prompt='Number of guardians', help='The number of guardians that will participate in the key ceremony and tally.', type=click.INT)
+@click.option('--quorum', prompt='Quorum', help='The minimum number of guardians required to show up to the tally.', type=click.INT)
+
 def start(guardian_count, quorum):
     """Runs through an end-to-end election."""
 
+    def print_header(s: str):
+        click.secho(f"{'-'*40}", fg='green')
+        click.secho(s, fg='green')
+        click.secho(f"{'-'*40}", fg='green')
+
+    def print_value(name: str, value: any):
+        click.echo(click.style(name + ": ") + click.style(value, fg='yellow'))
+
     manifest: Manifest = ElectionFactory().get_simple_manifest_from_file()
-    click.echo(f"{'-'*40}")
-    click.echo("Election Summary:")
+    
     manifest_name = manifest.name.text[0].value
-    click.echo(f"Name: {manifest_name}")
-    click.echo(f"Scope: {manifest.election_scope_id}")
-    click.echo(f"Geopolitical Units: {len(manifest.geopolitical_units)}")
-    click.echo(f"Parties: {len(manifest.parties)}")
-    click.echo(f"Candidates: {len(manifest.candidates)}")
-    click.echo(f"Contests: {len(manifest.contests)}")
-    click.echo(f"Ballot Styles: {len(manifest.ballot_styles)}")
-    click.echo(f"Guardians: {guardian_count}")
-    click.echo(f"Quorum: {quorum}")
-    click.echo(f"{'-'*40}\n")
+    print_header('Election Summary')
+    print_value("Name", manifest_name)
+    print_value("Scope", manifest.election_scope_id)
+    print_value("Geopolitical Units", len(manifest.geopolitical_units))
+    print_value("Parties", len(manifest.parties))
+    print_value("Candidates", len(manifest.candidates))
+    print_value("Contests", len(manifest.contests))
+    print_value("Ballot Styles", len(manifest.ballot_styles))
+    print_value("Guardians", guardian_count)
+    print_value("Quorum", quorum)
 
     if (not manifest.is_valid()):
         click.echo('bad bad bad')
