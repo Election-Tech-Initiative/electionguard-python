3fb718157fbd5c0f9dbb93196ba7ac16ecd4f220
Lee Richardson <github@leerichardson.com>
2022-04-04 15:01:00 -0400

    Refactor extract input retrieval to its own step

--- src/electionguard_cli/e2e_steps/__init__.py
+++ src/electionguard_cli/e2e_steps/__init__.py
@@ -3,11 +3,13 @@ from electionguard_cli.e2e_steps.election_builder_step import ElectionBuilderSte
 from electionguard_cli.e2e_steps.submit_votes_step import SubmitVotesStep
 from electionguard_cli.e2e_steps.decrypt_step import DecryptStep
 from electionguard_cli.e2e_steps.print_results_step import PrintResultsStep
+from electionguard_cli.e2e_steps.input_retrieval_step import InputRetrievalStep
 
 __all__ = [
     "ElectionBuilderStep",
     "KeyCeremonyStep",
     "SubmitVotesStep",
     "DecryptStep",
-    "PrintResultsStep"
+    "PrintResultsStep",
+    "InputRetrievalStep"
 ]
