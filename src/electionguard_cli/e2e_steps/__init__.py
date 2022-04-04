15ef3433a36e87a2152ade746b5568d4060f75d0
Lee Richardson <github@leerichardson.com>
2022-04-04 13:56:29 -0400

    Refactor vote submission into its own class/file

--- src/electionguard_cli/e2e_steps/__init__.py
+++ src/electionguard_cli/e2e_steps/__init__.py
@@ -1,7 +1,9 @@
 from electionguard_cli.e2e_steps.key_ceremony_step import KeyCeremonyStep
 from electionguard_cli.e2e_steps.election_builder_step import ElectionBuilderStep
+from electionguard_cli.e2e_steps.submit_votes_step import SubmitVotesStep
 
 __all__ = [
     "ElectionBuilderStep",
-    "KeyCeremonyStep"
+    "KeyCeremonyStep",
+    "SubmitVotesStep"
 ]
