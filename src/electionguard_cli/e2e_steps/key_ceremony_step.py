cea036eff88d7f38ff567634e74db995d5bfa13f
Lee Richardson <github@leerichardson.com>
2022-04-04 12:41:17 -0400

    Refactoring to fix linting issues

--- src/electionguard_cli/e2e_steps/key_ceremony_step.py
+++ src/electionguard_cli/e2e_steps/key_ceremony_step.py
@@ -1,12 +1,21 @@
+from typing import List
 from e2e_steps.e2e_step_base import E2eStepBase
-from electionguard.key_ceremony import CeremonyDetails, ElectionPublicKey
+from electionguard.key_ceremony import (
+    CeremonyDetails,
+    ElectionJointKey,
+    ElectionPublicKey,
+)
 from electionguard.guardian import Guardian
 from electionguard.key_ceremony_mediator import KeyCeremonyMediator
 from electionguard.utils import get_optional
-from typing import List
+
 
 class KeyCeremonyStep(E2eStepBase):
-    def __announce_guardians(mediator: KeyCeremonyMediator, guardians: List[Guardian]) -> List[ElectionPublicKey]:
+    """Responsible for running a key ceremony and producing an elgamal public key given a list of guardians."""
+
+    def __announce_guardians(
+        self, mediator: KeyCeremonyMediator, guardians: List[Guardian]
+    ) -> List[ElectionPublicKey]:
         for guardian in guardians:
             mediator.announce(guardian.share_key())
         return get_optional(mediator.share_announced())
@@ -17,14 +26,17 @@ class KeyCeremonyStep(E2eStepBase):
         )
         return mediator
 
-    def __share_Keys(self, announced_keys: List[ElectionPublicKey], guardians: List[Guardian]) -> None:
+    def __share_Keys(
+        self, announced_keys: List[ElectionPublicKey], guardians: List[Guardian]
+    ) -> None:
         for guardian in guardians:
             for key in announced_keys:
                 if guardian.id is not key.owner_id:
                     guardian.save_guardian_key(key)
-        
 
-    def __share_backups(self, mediator: KeyCeremonyMediator, guardians: List[Guardian]) -> None:
+    def __share_backups(
+        self, mediator: KeyCeremonyMediator, guardians: List[Guardian]
+    ) -> None:
         for sending_guardian in guardians:
             sending_guardian.generate_election_partial_key_backups()
             backups = []
@@ -39,14 +51,17 @@ class KeyCeremonyStep(E2eStepBase):
                     )
             mediator.receive_backups(backups)
 
-
-    def __receive_backups(self, mediator: KeyCeremonyMediator, guardians: List[Guardian]) -> None:
+    def __receive_backups(
+        self, mediator: KeyCeremonyMediator, guardians: List[Guardian]
+    ) -> None:
         for designated_guardian in guardians:
             backups = get_optional(mediator.share_backups(designated_guardian.id))
             for backup in backups:
                 designated_guardian.save_election_partial_key_backup(backup)
 
-    def __verify_backups(self, mediator: KeyCeremonyMediator, guardians: List[Guardian]) -> None:
+    def __verify_backups(
+        self, mediator: KeyCeremonyMediator, guardians: List[Guardian]
+    ) -> None:
         for designated_guardian in guardians:
             verifications = []
             for backup_owner in guardians:
@@ -59,18 +74,20 @@ class KeyCeremonyStep(E2eStepBase):
                     verifications.append(get_optional(verification))
             mediator.receive_backup_verifications(verifications)
 
-    def run_key_ceremony(self, guardians: List[Guardian]) -> None:
+    def run_key_ceremony(self, guardians: List[Guardian]) -> ElectionJointKey:
         self.print_header("Performing key ceremony")
 
-        mediator: KeyCeremonyMediator = self.__get_mediator(guardians[0].ceremony_details)
+        mediator: KeyCeremonyMediator = self.__get_mediator(
+            guardians[0].ceremony_details
+        )
         announced_keys = self.__announce_guardians(mediator, guardians)
         self.__share_Keys(announced_keys, guardians)
-        if (not mediator.all_guardians_announced()):
-            raise ValueError('All guardians failed to announce successfully')
+        if not mediator.all_guardians_announced():
+            raise ValueError("All guardians failed to announce successfully")
         self.__share_backups(mediator, guardians)
         self.__receive_backups(mediator, guardians)
         self.__verify_backups(mediator, guardians)
         joint_key = mediator.publish_joint_key()
 
-        self.print_value("Joint Key", joint_key.joint_public_key)
+        self.print_value("Joint Key", get_optional(joint_key).joint_public_key)
         return get_optional(joint_key)
