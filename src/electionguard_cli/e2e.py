9107d755ddc46220aa2e64f71707138670de9b9a
Lee Richardson <github@leerichardson.com>
2022-04-04 15:01:00 -0400

    Refactor extract input retrieval to its own step

--- src/electionguard_cli/e2e.py
+++ src/electionguard_cli/e2e.py
@@ -1,15 +1,5 @@
-from typing import List, Any, Optional
 import click
-from e2e_steps import ElectionBuilderStep, KeyCeremonyStep, SubmitVotesStep, DecryptStep, PrintResultsStep
-from electionguard.ballot import PlaintextBallot
-
-from electionguard.guardian import Guardian
-from electionguard.manifest import InternationalizedText, Manifest
-from electionguard_tools.factories.ballot_factory import BallotFactory
-from electionguard_tools.factories.election_factory import (
-    ElectionFactory,
-)
-
+from e2e_steps import ElectionBuilderStep, KeyCeremonyStep, SubmitVotesStep, DecryptStep, PrintResultsStep, InputRetrievalStep
 
 @click.command()
 @click.option(
@@ -27,63 +17,8 @@ from electionguard_tools.factories.election_factory import (
 def e2e(guardian_count: int, quorum: int) -> None:
     """Runs through an end-to-end election."""
 
-    def print_header(s: str) -> None:
-        click.secho(f"{'-'*40}", fg="green")
-        click.secho(s, fg="green")
-        click.secho(f"{'-'*40}", fg="green")
-
-    def print_value(name: str, value: Any) -> None:
-        click.echo(click.style(name + ": ") + click.style(value, fg="yellow"))
-
-    def get_ballots() -> List[PlaintextBallot]:
-        # todo: parameterize the plaintext ballot file
-        return BallotFactory().get_simple_ballots_from_file()
-
-    def print_manifest(manifest: Manifest) -> None:
-        def get_first_value(text: Optional[InternationalizedText]) -> str:
-            return "" if text is None else text.text[0].value
-
-        manifest_name = get_first_value(manifest.name)
-        print_value("Name", manifest_name)
-        print_value("Scope", manifest.election_scope_id)
-        print_value("Geopolitical Units", len(manifest.geopolitical_units))
-        print_value("Parties", len(manifest.parties))
-        print_value("Candidates", len(manifest.candidates))
-        print_value("Contests", len(manifest.contests))
-        print_value("Ballot Styles", len(manifest.ballot_styles))
-        print_value("Guardians", guardian_count)
-        print_value("Quorum", quorum)
-
-    def get_manifest() -> Manifest:
-        # todo: get manifest from params
-        print_header("Retrieving manifest")
-        manifest: Manifest = ElectionFactory().get_simple_manifest_from_file()
-        if not manifest.is_valid():
-            raise ValueError("manifest file is invalid")
-        print_manifest(manifest)
-        return manifest
-
-    def get_guardians(number_of_guardians: int) -> List[Guardian]:
-        guardians: List[Guardian] = []
-        for i in range(number_of_guardians):
-            guardians.append(
-                Guardian(
-                    str(i + 1),
-                    i + 1,
-                    number_of_guardians,
-                    quorum,
-                )
-            )
-        return guardians
-
-
-
-
-
     # get user inputs
-    guardians = get_guardians(guardian_count)
-    manifest: Manifest = get_manifest()
-    ballots = get_ballots()
+    (guardians, manifest, ballots) = InputRetrievalStep().get_inputs(guardian_count, quorum)
 
     # perform election
     joint_key = KeyCeremonyStep().run_key_ceremony(guardians)
---
+++ src/electionguard_cli/e2e_steps/input_retrieval_step.py
@@ -0,0 +1,63 @@
+from typing import List, Optional, Tuple
+from e2e_steps.e2e_step_base import E2eStepBase
+from electionguard.ballot import PlaintextBallot
+
+from electionguard.guardian import Guardian
+from electionguard.manifest import InternationalizedText, Manifest
+from electionguard_tools.factories.ballot_factory import BallotFactory
+from electionguard_tools.factories.election_factory import (
+    ElectionFactory,
+)
+
+
+class InputRetrievalStep(E2eStepBase):
+    """Responsible for retrieving and displaying user provided inputs"""
+
+    def get_ballots(self) -> List[PlaintextBallot]:
+        # todo: parameterize the plaintext ballot file
+        ballots: List[PlaintextBallot] = BallotFactory().get_simple_ballots_from_file()
+        return ballots
+
+    def print_manifest(self, manifest: Manifest) -> None:
+        def get_first_value(text: Optional[InternationalizedText]) -> str:
+            return "" if text is None else text.text[0].value
+
+        manifest_name = get_first_value(manifest.name)
+        self.print_value("Name", manifest_name)
+        self.print_value("Scope", manifest.election_scope_id)
+        self.print_value("Geopolitical Units", len(manifest.geopolitical_units))
+        self.print_value("Parties", len(manifest.parties))
+        self.print_value("Candidates", len(manifest.candidates))
+        self.print_value("Contests", len(manifest.contests))
+        self.print_value("Ballot Styles", len(manifest.ballot_styles))
+
+    def get_manifest(self) -> Manifest:
+        # todo: get manifest from params
+        self.print_header("Retrieving manifest")
+        manifest: Manifest = ElectionFactory().get_simple_manifest_from_file()
+        if not manifest.is_valid():
+            raise ValueError("manifest file is invalid")
+        self.print_manifest(manifest)
+        return manifest
+
+    def get_guardians(self, number_of_guardians: int, quorum: int) -> List[Guardian]:
+        guardians: List[Guardian] = []
+        for i in range(number_of_guardians):
+            guardians.append(
+                Guardian(
+                    str(i + 1),
+                    i + 1,
+                    number_of_guardians,
+                    quorum,
+                )
+            )
+        return guardians
+
+    def get_inputs(self, guardian_count: int, quorum: int) -> Tuple[List[Guardian], Manifest, List[PlaintextBallot]]:
+        self.print_header("Retrieving Inputs")
+        guardians = self.get_guardians(guardian_count, quorum)
+        manifest: Manifest = self.get_manifest()
+        ballots = self.get_ballots()
+        self.print_value("Guardians", guardian_count)
+        self.print_value("Quorum", quorum)
+        return (guardians, manifest, ballots)
