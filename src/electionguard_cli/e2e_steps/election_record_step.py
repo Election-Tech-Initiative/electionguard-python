c2d3b36b23b9afda6740d165e924782636fe32a9
Lee Richardson <github@leerichardson.com>
2022-04-11 14:13:01 -0400

    Refactor election record and private data into separate directories

--- src/electionguard_cli/e2e_steps/election_record_step.py
+++ src/electionguard_cli/e2e_steps/election_record_step.py
@@ -1,5 +1,4 @@
 from shutil import rmtree, make_archive
-from os import remove
 import click
 from electionguard_cli.cli_models import BuildElectionResults, E2eSubmitResults
 from electionguard_cli.cli_models.e2e_decrypt_results import E2eDecryptResults
@@ -8,8 +7,8 @@ from electionguard_cli.e2e_steps.e2e_step_base import E2eStepBase
 from electionguard.constants import get_constants
 
 from electionguard_tools.helpers.export import (
-    PRIVATE_DATA_DIR,
     ELECTION_RECORD_DIR,
+    PRIVATE_DATA_DIR,
     export_private_data,
     export_record,
 )
@@ -20,6 +19,7 @@ class ElectionRecordStep(E2eStepBase):
 
     REMOVE_RAW_OUTPUT = True
     REMOVE_ZIP_OUTPUT = True
+    COMPRESSION_FORMAT = "zip"
 
     def run(
         self,
@@ -28,15 +28,24 @@ class ElectionRecordStep(E2eStepBase):
         submit_results: E2eSubmitResults,
         decrypt_results: E2eDecryptResults,
     ) -> None:
+
         self.print_header("Election Record")
-        click.echo(f"Exporting to {election_inputs.output_path}")
+        self._export_election_record(
+            election_inputs, build_election_results, submit_results, decrypt_results
+        )
+        self._export_private_data(election_inputs, submit_results)
+
+    def _export_election_record(
+        self,
+        election_inputs: E2eInputs,
+        build_election_results: BuildElectionResults,
+        submit_results: E2eSubmitResults,
+        decrypt_results: E2eDecryptResults,
+    ) -> None:
+
         guardian_records = [
             guardian.publish() for guardian in election_inputs.guardians
         ]
-        private_guardian_records = [
-            guardian.export_private_data() for guardian in election_inputs.guardians
-        ]
-
         constants = get_constants()
         export_record(
             election_inputs.manifest,
@@ -49,23 +58,29 @@ class ElectionRecordStep(E2eStepBase):
             decrypt_results.plaintext_tally,
             guardian_records,
             decrypt_results.lagrange_coefficients,
+            election_record_directory=ELECTION_RECORD_DIR,
         )
+        make_archive(ELECTION_RECORD_DIR, self.COMPRESSION_FORMAT, ELECTION_RECORD_DIR)
+        if self.REMOVE_RAW_OUTPUT:
+            rmtree(ELECTION_RECORD_DIR)
 
+    def _export_private_data(
+        self,
+        election_inputs: E2eInputs,
+        submit_results: E2eSubmitResults,
+    ) -> None:
+        private_guardian_records = [
+            guardian.export_private_data() for guardian in election_inputs.guardians
+        ]
         export_private_data(
             election_inputs.ballots,
             submit_results.ciphertext_ballots,
             private_guardian_records,
         )
         click.echo(f"Private data exported to: {PRIVATE_DATA_DIR}")
+        make_archive(PRIVATE_DATA_DIR, self.COMPRESSION_FORMAT, PRIVATE_DATA_DIR)
 
-        ZIP_SUFFIX = "zip"
-        make_archive(ELECTION_RECORD_DIR, ZIP_SUFFIX, ELECTION_RECORD_DIR)
-        make_archive(PRIVATE_DATA_DIR, ZIP_SUFFIX, PRIVATE_DATA_DIR)
+        click.echo(f"Exporting to {election_inputs.output_path}")
 
         if self.REMOVE_RAW_OUTPUT:
-            rmtree(ELECTION_RECORD_DIR)
             rmtree(PRIVATE_DATA_DIR)
-
-        if self.REMOVE_ZIP_OUTPUT:
-            remove(f"{ELECTION_RECORD_DIR}.{ZIP_SUFFIX}")
-            remove(f"{PRIVATE_DATA_DIR}.{ZIP_SUFFIX}")
