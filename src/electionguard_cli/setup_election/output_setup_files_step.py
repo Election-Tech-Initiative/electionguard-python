78928da3279365731a787eab724f070f6a78883d
Lee Richardson <github@leerichardson.com>
2022-04-27 13:55:26 -0400

    Export context

--- src/electionguard_cli/setup_election/output_setup_files_step.py
+++ src/electionguard_cli/setup_election/output_setup_files_step.py
@@ -1,7 +1,8 @@
 from os.path import join
+from electionguard.election import CiphertextElectionContext
 from electionguard.serialize import to_file
 from electionguard.constants import get_constants
-from electionguard_tools.helpers.export import CONSTANTS_FILE_NAME
+from electionguard_tools.helpers.export import CONSTANTS_FILE_NAME, CONTEXT_FILE_NAME
 
 from .setup_inputs import SetupInputs
 from ..cli_models.e2e_build_election_results import BuildElectionResults
@@ -16,11 +17,18 @@ class OutputSetupFilesStep(OutputStepBase):
     ) -> None:
         self.print_header("Generating Output")
 
-        # todo: output CiphertextElectionContext.json
+        self._export_context(setup_inputs, build_election_results.context)
         self._export_constants(setup_inputs)
         # todo: GuardianRecord.json
 
         self._export_guardian_private_keys(setup_inputs)
+
+    def _export_context(
+        self, setup_inputs: SetupInputs, context: CiphertextElectionContext
+    ) -> None:
+        to_file(context, CONTEXT_FILE_NAME, setup_inputs.out)
+        location = join(setup_inputs.out, CONTEXT_FILE_NAME + ".json")
+        self.print_value("Context", location)
 
     def _export_constants(self, setup_inputs: SetupInputs) -> None:
         constants = get_constants()
