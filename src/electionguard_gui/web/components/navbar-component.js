7254ae17f491ac98037389ab70a456928d849970
Lee Richardson <github@leerichardson.com>
2022-06-07 18:08:12 -0400

    Rewrite app with vue and components

---
+++ src/electionguard_gui/web/components/navbar-component.js
@@ -0,0 +1,31 @@
+export default {
+  template: `<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img
+        src="images/electionguard-icon.svg"
+        height="30"
+        class="d-inline-block align-text-top"
+      />
+      ElectionGuard
+    </a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="page2.html">Page 2</a>
+      </div>
+    </div>
+  </div>
+</nav>`,
+};
---
+++ src/electionguard_gui/web/components/setup-election-component.js
@@ -0,0 +1,82 @@
+export default {
+  methods: {
+    setupElection() {
+      const form = document.getElementById("mainForm");
+      if (form.checkValidity()) {
+        const guardianCount = getIntById("guardianCount");
+        const quorum = getIntById("quorum");
+        const manifest = document.getElementById("manifest").files[0];
+        var reader = new FileReader();
+        reader.onloadend = (e) => {
+          const manifestContent = e.target.result;
+          const onSuccess = eel.setup_election(
+            guardianCount,
+            quorum,
+            manifestContent
+          );
+          console.log("setting up election");
+          onSuccess((manifest_raw) => {
+            var a = document.createElement("a");
+            a.href = window.URL.createObjectURL(
+              new Blob([manifest_raw], { type: "text/plain" })
+            );
+            a.download = "context.json";
+            a.click();
+            console.log("done!", r);
+          });
+        };
+        reader.readAsText(manifest);
+      }
+      form.classList.add("was-validated");
+    },
+  },
+  template: `<form id="mainForm" class="needs-validation" novalidate @submit.prevent="setupElection">
+  <div class="row g-3 align-items-center">
+    <div class="col-12">
+      <h1>Setup Election</h1>
+    </div>
+    <div class="col-sm-6">
+      <label for="guardianCount" class="form-label">Number of Guardians</label>
+      <input
+        type="number"
+        class="form-control"
+        id="guardianCount"
+        value="2"
+        required
+      />
+      <div class="invalid-feedback">
+        Please provide a valid number of guardians.
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <label for="quorum" class="form-label">Quorum</label>
+      <input
+        type="number"
+        class="form-control"
+        id="quorum"
+        value="2"
+        required
+      />
+      <div class="invalid-feedback">Please provide a valid quorum.</div>
+    </div>
+    <div class="col-12">
+      <label for="manifest" class="form-label">Manifest</label>
+      <input
+        type="file"
+        id="manifest"
+        class="form-control"
+        required
+      />
+      <div class="invalid-feedback">Please provide a valid manifest.</div>
+    </div>
+    <div class="col-12 mt-4">
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>`,
+};
+
+function getIntById(str) {
+  const valueStr = document.getElementById(str).value;
+  return Number.parseInt(valueStr);
+}
