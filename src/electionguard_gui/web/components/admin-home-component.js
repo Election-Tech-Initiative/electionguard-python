0e63320c47d7c17101afc2882e9a6f4582214317
Lee Richardson <github@leerichardson.com>
2022-06-08 18:33:34 -0400

    ui cleanup

--- src/electionguard_gui/web/components/admin-home-component.js
+++ src/electionguard_gui/web/components/admin-home-component.js
@@ -1,8 +1,18 @@
 export default {
   emits: ["navigate"],
   template: `
-  <h1>Admin Menu</h1>
-  <button type="button" class="btn btn-primary" @click="$emit('navigate', 'create-key')">Create Key</button>
-  <button type="button" class="btn btn-primary" @click="$emit('navigate', 'setup-election')">Setup Election</button>
+  <div class="container col-6">
+    <div class="text-center mb-4">
+      <h1>Admin Menu</h1>
+    </div>
+    <div class="row justify-content-md-center">
+      <div class="col-12 d-grid mb-3">
+        <button type="button" class="btn btn-primary" @click="$emit('navigate', 'create-key')">Create Key</button>
+      </div>
+      <div class="col-12 d-grid">
+        <button type="button" class="btn btn-primary" @click="$emit('navigate', 'setup-election')">Setup Election</button>
+      </div>
+    </div>
+  </div>
   `,
 };
--- src/electionguard_gui/web/components/select-mode-component.js
+++ src/electionguard_gui/web/components/select-mode-component.js
@@ -1,8 +1,16 @@
 export default {
   emits: ["navigate"],
   template: `
-  <h1>Mode Selection</h1>
-  <button type="button" class="btn btn-primary" @click="$emit('navigate', 'admin-home')">Administrator</button>
-  <button type="button" class="btn btn-primary" @click="$emit('navigate', 'guardian-home')">Guardian</button>
+  <div class="text-center mb-4">
+    <h1>Mode Selection</h1>
+  </div>
+  <div class="row justify-content-md-center">
+    <div class="col-3 d-grid gap-2">
+      <button type="button" class="btn btn-primary" @click="$emit('navigate', 'admin-home')">Administrator</button>
+    </div>
+    <div class="col-3 d-grid gap-2">
+      <button type="button" class="btn btn-primary" @click="$emit('navigate', 'guardian-home')">Guardian</button>
+    </div>
+  </div>
   `,
 };
