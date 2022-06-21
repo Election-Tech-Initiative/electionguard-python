29828927fa5af0a8b4ba6c6fda52f937a022fa3c
Lee Richardson <github@leerichardson.com>
2022-06-21 09:35:33 -0400

    "Push" joinable keys from admin to guardian (via polling to db)

--- src/electionguard_gui/start.py
+++ src/electionguard_gui/start.py
@@ -1,5 +1,6 @@
 from time import sleep
 from typing import Any
+from threading import Event
 import eel
 from pymongo import MongoClient
 from pymongo.database import Database
@@ -15,17 +16,15 @@ from electionguard_gui.gui_setup_election.gui_setup_input_retrieval_step import 
     GuiSetupInputRetrievalStep,
 )
 
+watching_keys = Event()
+
 
 @eel.expose
 def start_ceremony(key_name: str, guardian_count: int, quorum: int) -> dict[str, Any]:
     print(
         f"Starting ceremony: key_name: {key_name}, guardian_count: {guardian_count}, quorum: {quorum}"
     )
-    # todo: parameterize db credentials here and in docker-compose.db.yml
-    client: MongoClient = MongoClient(
-        "localhost", 27017, username="root", password="example"
-    )
-    db: Database = client.ElectionGuardDb
+    db = get_db()
     keys_collection = db.keys
     existing_keys = keys_collection.find_one({"key_name": key_name})
     if existing_keys:
@@ -44,12 +43,21 @@ def start_ceremony(key_name: str, guardian_count: int, quorum: int) -> dict[str,
     return eel_success()
 
 
+def get_db() -> Database:
+    # todo: parameterize db credentials here and in docker-compose.db.yml
+    client: MongoClient = MongoClient(
+        "localhost", 27017, username="root", password="example"
+    )
+    db: Database = client.ElectionGuardDb
+    return db
+
+
 def eel_fail(message: str) -> dict[str, Any]:
     return {"success": False, "message": message}
 
 
-def eel_success() -> dict[str, Any]:
-    return {"success": True}
+def eel_success(result: Any = None) -> dict[str, Any]:
+    return {"success": True, "result": result}
 
 
 @eel.expose
@@ -65,9 +73,37 @@ def setup_election(guardian_count: int, quorum: int, manifest: str) -> str:
     context_file = files[0]
     constants_file = files[1]
     print(f"Setup complete, context: {context_file}, constants: {constants_file}")
+    # todo: refactor to eel_success()
     with open(context_file, "r", encoding="utf-8") as context_file:
         context_raw: str = context_file.read()
         return context_raw
+
+
+@eel.expose
+def watch_keys() -> None:
+    db = get_db()
+    last_count = 0
+    watching_keys.set()
+    while watching_keys.is_set():
+        current_count = db.keys.count_documents({})
+        print(f"polling, guardian count = {current_count}")
+        if current_count != last_count:
+            print(f"found new keys. Count was {last_count}, and is now {current_count}")
+            keys = db.keys.find()
+            js_keys = []
+            for key in keys:
+                js_keys.append(key)
+            eel.keys_found(js_keys)
+            print("called keys_found successfully")
+        last_count = current_count
+        eel.sleep(1.0)
+    print("exited watch keys loop")
+
+
+@eel.expose
+def stop_watching() -> None:
+    print("stop_watching")
+    watching_keys.clear()
 
 
 def run() -> None:
--- src/electionguard_gui/web/components/guardian-home-component.js
+++ src/electionguard_gui/web/components/guardian-home-component.js
@@ -1,5 +1,35 @@
 export default {
+  data() {
+    return {
+      keys: [],
+    };
+  },
+  methods: {
+    keys_found: function (keys) {
+      console.log("found keys", keys);
+      this.keys = keys;
+    },
+  },
+  mounted() {
+    console.log("begin watching for keys");
+    eel.expose(this.keys_found, "keys_found");
+    eel.watch_keys();
+  },
+  unmounted() {
+    console.log("stop watching keys");
+    eel.stop_watching();
+    // todo: un-expose "keys_found" ?
+  },
   template: /*html*/ `
-  <h1>Hello Guardian</h1>
+  <h1>Guardian Home</h1>
+  <h2>Keys to Join</h2>
+  <p v-if="!keys">No keys found...</p>
+  <p v-if="keys">
+    <ul class="list-unstyled">
+    <li v-for="key in keys">
+      <a href="#" class="btn btn-primary mt-2">{{ key.key_name }}</a>
+    </li>
+    </ul>
+  </p>
   `,
 };
