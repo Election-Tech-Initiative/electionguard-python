cb67b1ea31070cec61b9ba07e747b2c8539a59a4
Lee Richardson <github@leerichardson.com>
2022-06-28 12:25:19 -0400

    Ensure verification url gets into final package during import

--- src/electionguard_cli/import_ballots/import_ballots_election_builder_step.py
+++ src/electionguard_cli/import_ballots/import_ballots_election_builder_step.py
@@ -10,9 +10,12 @@ class ImportBallotsElectionBuilderStep(ElectionBuilderStep):
     def build_election_with_context(
         self, election_inputs: ImportBallotInputs
     ) -> BuildElectionResults:
+        verification_url = election_inputs.context.get_extended_data_field(
+            self.verification_url_name
+        )
         return self._build_election(
             election_inputs,
             election_inputs.context.elgamal_public_key,
             election_inputs.context.commitment_hash,
-            None,
+            verification_url,
         )
