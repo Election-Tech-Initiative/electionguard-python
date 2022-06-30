505c90255b64d32c41bd40c92efd7f7e4d99e62c
Keith Fung <keithrfung@users.noreply.github.com>
2022-06-30 12:01:38 -0400

    🐞 Refactor - CLI models should use dataclass instead of init (#670)
    dataclass autogenerates the exact same init since there is no specialization on these files. This shortens the classes down and simplifies constructors.
    
    Co-authored-by: Lee Richardson <github@leerichardson.com>

--- src/electionguard_cli/cli_models/encrypt_results.py
+++ src/electionguard_cli/cli_models/encrypt_results.py
@@ -1,18 +1,12 @@
+from dataclasses import dataclass
 from typing import List
 from electionguard.ballot import CiphertextBallot
 from electionguard.encrypt import EncryptionDevice
 
 
+@dataclass
 class EncryptResults:
     """Responsible for holding the results of encrypting votes in an election."""
-
-    def __init__(
-        self,
-        device: EncryptionDevice,
-        ciphertext_ballots: List[CiphertextBallot],
-    ):
-        self.device = device
-        self.ciphertext_ballots = ciphertext_ballots
 
     device: EncryptionDevice
     ciphertext_ballots: List[CiphertextBallot]
