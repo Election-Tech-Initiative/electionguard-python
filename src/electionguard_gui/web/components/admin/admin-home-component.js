c846436fbaae193045501f03546facbb46320d1d
Lee Richardson <github@leerichardson.com>
2022-07-06 09:11:19 -0400

    fix links

--- src/electionguard_gui/web/components/admin/admin-home-component.js
+++ src/electionguard_gui/web/components/admin/admin-home-component.js
@@ -6,10 +6,10 @@ export default {
     </div>
     <div class="row justify-content-md-center">
       <div class="col-12 d-grid mb-3">
-        <a href="#/create-key" class="btn btn-primary">Create Key</a>
+        <a href="#/admin/create-key" class="btn btn-primary">Create Key</a>
       </div>
       <div class="col-12 d-grid">
-        <a href="#/setup-election" class="btn btn-primary">Setup Election</a>
+        <a href="#/admin/setup-election" class="btn btn-primary">Setup Election</a>
       </div>
     </div>
   </div>`,
