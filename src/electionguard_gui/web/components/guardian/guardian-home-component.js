7a7c22bc849662188b652a18426e0944f8f557f6
Lee Richardson <github@leerichardson.com>
2022-07-08 15:24:59 -0400

    Tidy up buttons per PR

--- src/electionguard_gui/web/components/guardian/guardian-home-component.js
+++ src/electionguard_gui/web/components/guardian/guardian-home-component.js
@@ -28,12 +28,9 @@ export default {
   <h1>Guardian Home</h1>
   <h2>Key Ceremonies</h2>
   <p v-if="!keyCeremonies">No key ceremonies found...</p>
-  <p v-if="keyCeremonies">
-    <ul class="list-unstyled">
-    <li v-for="keyCeremony in keyCeremonies">
-      <button type="button" @click="join(keyCeremony.id)" class="btn btn-primary mt-2">{{ keyCeremony.key_ceremony_name }}</a>
-    </li>
-    </ul>
-  </p>
+
+  <div v-if="keyCeremonies" class="d-grid gap-2 d-md-block">
+    <button v-for="keyCeremony in keyCeremonies" type="button" @click="join(keyCeremony.id)" class="btn btn-primary me-2">{{ keyCeremony.key_ceremony_name }}</button>
+  </div>
   `,
 };
