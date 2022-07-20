5201fb0815749a97cfae85982c903ee654c871df
Lee Richardson <github@leerichardson.com>
2022-07-20 12:15:15 -0400

    Generate init.py's for the gui

---
+++ src/electionguard_gui/models/__init__.py
@@ -0,0 +1,16 @@
+from electionguard_gui.models import key_ceremony_dto
+from electionguard_gui.models import key_ceremony_states
+
+from electionguard_gui.models.key_ceremony_dto import (
+    KeyCeremonyDto,
+)
+from electionguard_gui.models.key_ceremony_states import (
+    KeyCeremonyStates,
+)
+
+__all__ = [
+    "KeyCeremonyDto",
+    "KeyCeremonyStates",
+    "key_ceremony_dto",
+    "key_ceremony_states",
+]
