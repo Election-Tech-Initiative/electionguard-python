bc1e89dfca2e68413ccdd91898c0d1e47f4dc62e
Lee Richardson <github@leerichardson.com>
2022-08-07 20:13:46 -0700

    Fix linting issues

--- tests/unit/electionguard_gui/test_decryption_dto.py
+++ tests/unit/electionguard_gui/test_decryption_dto.py
@@ -1,82 +1,82 @@
 from datetime import datetime
 
 from unittest.mock import MagicMock, patch
+
 from electionguard_gui.models.decryption_dto import DecryptionDto
 from tests.base_test_case import BaseTestCase
+
 
 class TestDecryptionDto(BaseTestCase):
     """Test the DecryptionDto class"""
 
     def test_get_status_with_no_guardians(self) -> None:
         # ARRANGE
-        decryption_dto = DecryptionDto({
-            "guardians_joined": [],
-            "guardians": 2,
-        })
-        
+        decryption_dto = DecryptionDto(
+            {
+                "guardians_joined": [],
+                "guardians": 2,
+            }
+        )
+
         # ACT
         status = decryption_dto.get_status()
-        
+
         # ASSERT
-        self.assertEquals(status, "waiting for all guardians to join")
+        self.assertEqual(status, "waiting for all guardians to join")
 
     def test_get_status_with_all_guardians_joined_but_not_completed(self) -> None:
         # ARRANGE
-        decryption_dto = DecryptionDto({
-            "guardians_joined": ["g1","g2"],
-            "guardians": 2,
-            "completed_at_utc": None
-        })
-        
+        decryption_dto = DecryptionDto(
+            {"guardians_joined": ["g1", "g2"], "guardians": 2, "completed_at_utc": None}
+        )
+
         # ACT
         status = decryption_dto.get_status()
-        
+
         # ASSERT
-        self.assertEquals(status, "performing decryption")
+        self.assertEqual(status, "performing decryption")
 
     def test_get_status_with_all_guardians_joined_and_completed(self) -> None:
         # ARRANGE
-        decryption_dto = DecryptionDto({
-            "guardians_joined": ["g1"],
-            "guardians": 1,
-            "completed_at": datetime.utcnow()
-        })
+        decryption_dto = DecryptionDto(
+            {
+                "guardians_joined": ["g1"],
+                "guardians": 1,
+                "completed_at": datetime.utcnow(),
+            }
+        )
 
         # ACT
         status = decryption_dto.get_status()
-        
+
         # ASSERT
-        self.assertEquals(status, "decryption complete")
+        self.assertEqual(status, "decryption complete")
 
     @patch("electionguard_gui.services.authorization_service.AuthorizationService")
     def test_admins_can_not_join_key_ceremony(self, auth_service: MagicMock):
         # ARRANGE
-        decryption_dto = DecryptionDto({
-            "guardians_joined": []
-        })
-        
-        auth_service.configure_mock(**{
-            "get_user_id.return_value": "admin1",
-            "is_admin.return_value": True
-        })
+        decryption_dto = DecryptionDto({"guardians_joined": []})
+
+        auth_service.configure_mock(
+            **{"get_user_id.return_value": "admin1", "is_admin.return_value": True}
+        )
 
         # ACT
         decryption_dto.set_can_join(auth_service)
-        
+
         # ASSERT
         self.assertFalse(decryption_dto.can_join)
 
     @patch("electionguard_gui.services.authorization_service.AuthorizationService")
-    def test_users_can_join_key_ceremony_if_not_already_joined(self, auth_service: MagicMock):
+    def test_users_can_join_key_ceremony_if_not_already_joined(
+        self, auth_service: MagicMock
+    ):
         # ARRANGE
-        decryption_dto = DecryptionDto({
-            "guardians_joined": []
-        })
-        
-        auth_service.configure_mock(**{
-            "get_user_id.return_value": "user1",
-            "is_admin.return_value": False
-        })
+        decryption_dto = DecryptionDto({"guardians_joined": []})
+
+        auth_service.configure_mock(
+            **{"get_user_id.return_value": "user1", "is_admin.return_value": False}
+        )
 
         # ACT
         decryption_dto.set_can_join(auth_service)
@@ -87,14 +87,11 @@ class TestDecryptionDto(BaseTestCase):
     @patch("electionguard_gui.services.authorization_service.AuthorizationService")
     def test_users_cant_join_twice(self, auth_service: MagicMock):
         # ARRANGE
-        decryption_dto = DecryptionDto({
-            "guardians_joined": ["user1"]
-        })
-        
-        auth_service.configure_mock(**{
-            "get_user_id.return_value": "user1",
-            "is_admin.return_value": False
-        })
+        decryption_dto = DecryptionDto({"guardians_joined": ["user1"]})
+
+        auth_service.configure_mock(
+            **{"get_user_id.return_value": "user1", "is_admin.return_value": False}
+        )
 
         # ACT
         decryption_dto.set_can_join(auth_service)
--- tests/unit/electionguard_gui/test_election_dto.py
+++ tests/unit/electionguard_gui/test_election_dto.py
@@ -1,35 +1,28 @@
 from datetime import datetime, timezone
 
-from unittest.mock import MagicMock, patch
-
-import pytest
-from pytest_mock import MockerFixture
 from electionguard_gui.models.election_dto import ElectionDto
 from tests.base_test_case import BaseTestCase
+
 
 class TestElectionDto(BaseTestCase):
     """Test the ElectionDto class"""
 
-    mocker: MockerFixture
-
-    @pytest.fixture(autouse=True)
-    def __inject_fixtures(self, mocker):
-        self.mocker = mocker
-
     def test_get_status_with_no_guardians(self) -> None:
         # ARRANGE
         self.mocker.patch(
-            "electionguard_gui.models.election_dto.utc_to_str", 
-            return_value="Feb 3, 2022 2:10 PM"
+            "electionguard_gui.models.election_dto.utc_to_str",
+            return_value="Feb 3, 2022 2:10 PM",
         )
-        election_dto = ElectionDto({
-            "_id": "ABC",
-            "created_at": datetime(2020, 2, 3, 7, 10, 10, 0, tzinfo=timezone.utc),
-        })
+        election_dto = ElectionDto(
+            {
+                "_id": "ABC",
+                "created_at": datetime(2020, 2, 3, 7, 10, 10, 0, tzinfo=timezone.utc),
+            }
+        )
 
         # ACT
         result = election_dto.to_dict()
 
         # ASSERT
-        self.assertEquals("ABC", result["id"])
-        self.assertEquals(result["created_at"], "Feb 3, 2022 2:10 PM")
+        self.assertEqual("ABC", result["id"])
+        self.assertEqual(result["created_at"], "Feb 3, 2022 2:10 PM")
