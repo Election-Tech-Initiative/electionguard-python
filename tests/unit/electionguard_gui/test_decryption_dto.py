4ac2e58459a41dfc19108bac6470119ad35fa71d
Lee Richardson <github@leerichardson.com>
2022-08-07 18:33:14 -0700

    Test that admins can never join a key ceremony

--- tests/unit/electionguard_gui/test_decryption_dto.py
+++ tests/unit/electionguard_gui/test_decryption_dto.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+
+from pytest_mock import MockFixture
+from unittest.mock import patch
 from electionguard_gui.models.decryption_dto import DecryptionDto
 from tests.base_test_case import BaseTestCase
 
@@ -30,3 +33,16 @@ class TestDecryptionDto(BaseTestCase):
         })
         status = decryption_dto.get_status()
         self.assertEquals(status, "decryption complete")
+
+    @patch("electionguard_gui.services.authorization_service.AuthorizationService")
+    def test_admins_can_not_join_key_ceremony(self, auth_service: MockFixture):
+        decryption_dto = DecryptionDto({
+            "guardians_joined": []
+        })
+        
+        auth_service.configure_mock(**{
+            "get_user_id.return_value": "admin1",
+            "is_admin.return_value": True
+        })
+        actual = decryption_dto.set_can_join(auth_service)
+        self.assertFalse(actual)
