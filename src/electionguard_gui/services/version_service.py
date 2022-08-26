96e4f0cc4492475f099b5a7594c7b746cb97f632
Lee Richardson <github@leerichardson.com>
2022-08-26 13:17:45 -0400

    Don't bother showing the version info on docker

--- src/electionguard_gui/services/version_service.py
+++ src/electionguard_gui/services/version_service.py
@@ -1,4 +1,5 @@
 from os import path
+from typing import Optional
 import eel
 from git import Repo
 from electionguard_gui.services.eel_log_service import EelLogService
@@ -16,12 +17,11 @@ class VersionService(ServiceBase):
     def expose(self) -> None:
         eel.expose(self.get_version)
 
-    def get_version(self) -> str:
-        if path.exists(".git"):
-            repo = Repo("./")
-            commit_hash = repo.git.rev_parse("HEAD")
-            short_hash = commit_hash[:6]
-            self._log.info(f"Version: {short_hash}")
-            return short_hash
-        else:
-            return "unknown"
+    def get_version(self) -> Optional[str]:
+        if not path.exists(".git"):
+            return None
+        repo = Repo("./")
+        commit_hash = repo.git.rev_parse("HEAD")
+        short_hash = commit_hash[:6]
+        self._log.info(f"Version: {short_hash}")
+        return short_hash
--- src/electionguard_gui/web/components/shared/footer-component.js
+++ src/electionguard_gui/web/components/shared/footer-component.js
@@ -8,9 +8,9 @@ export default {
     this.version = await eel.get_version()();
   },
   template: /*html*/ `
-  <nav class="navbar fixed-bottom bg-light">
+  <nav class="navbar fixed-bottom bg-light" v-if="version">
     <div class="container-fluid">
-        <div class="col-12 pe-0 text-end text-secondary text-opacity-75" v-if="version">
+        <div class="col-12 pe-0 text-end text-secondary text-opacity-75">
           ElectionGuard version {{version}}
         </div>
     </div>
